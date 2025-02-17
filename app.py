a20ddcf87e210887687361e43f3f77714dd682e3
LostRishi <dps.raj2011@gmail.com>
2025-02-17 11:07:21 +0530

    removed langsmith tracking

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Langsmith Tracking
-os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
-os.environ['LANGCHAIN_TRACING_V2']='true'
-os.environ['LANGCHAIN_PROJECT'] = "Q&A chatbot with OpenAI"
+# os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
+# os.environ['LANGCHAIN_TRACING_V2']='true'
+# os.environ['LANGCHAIN_PROJECT'] = "Q&A chatbot with OpenAI"
 
 
 ## Prompt template
